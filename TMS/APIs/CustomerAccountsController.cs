884ef2283cc439ff3e6659da2800da618ee6986c
Pencilate <kazukitsukino@gmail.com>
2019-12-12 00:46:46 +0800

    Made Customer Account Controller have more specific status code respoonse

--- TMS/APIs/CustomerAccountsController.cs
+++ TMS/APIs/CustomerAccountsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
@@ -33,22 +34,31 @@ namespace TMS.APIs
         [HttpGet]
         public IActionResult Get()
         {
-            List<CustomerAccount> cusAccQueryList = Database.CustomerAccounts.Include(c => c.CreatedBy).Include(c => c.UpdatedBy).ToList();
             List<Object> cusAccList = new List<object>();
+            try
+            {
+                List<CustomerAccount> cusAccQueryList = Database.CustomerAccounts.Include(c => c.CreatedBy).Include(c => c.UpdatedBy).ToList();
 
-            foreach(CustomerAccount ca in cusAccQueryList){
-                cusAccList.Add(new {
-                    id = ca.CustomerAccountId,
-                    accountName = ca.AccountName,
-                    //comments = ca.Comments.Count,
-                    comments = Database.CustomerAccountComments.Where(c => c.UpdatedAt.CompareTo(_appDateTimeService.GetCurrentDateTime().Date.AddDays(-3)) > 0).Count(c => c.CustomerAccountId == ca.CustomerAccountId),
-                    visibility = ca.IsVisible,
-                    createdBy = ca.CreatedBy.FullName,
-                    updatedBy = ca.UpdatedBy.FullName,
-                    updatedAt = ca.UpdatedAt
-                });
+
+                foreach (CustomerAccount ca in cusAccQueryList)
+                {
+                    cusAccList.Add(new
+                    {
+                        id = ca.CustomerAccountId,
+                        accountName = ca.AccountName,
+                        //comments = ca.Comments.Count,
+                        comments = Database.CustomerAccountComments.Where(c => c.UpdatedAt.CompareTo(_appDateTimeService.GetCurrentDateTime().Date.AddDays(-3)) > 0).Count(c => c.CustomerAccountId == ca.CustomerAccountId),
+                        visibility = ca.IsVisible,
+                        createdBy = ca.CreatedBy.FullName,
+                        updatedBy = ca.UpdatedBy.FullName,
+                        updatedAt = ca.UpdatedAt
+                    });
+                }
             }
-
+            catch (SqlException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "Something wrong has occured. Please contact the administrators." });
+            }
             return Ok(cusAccList);
         }
 
@@ -65,51 +75,57 @@ namespace TMS.APIs
         public IActionResult Post([FromForm] IFormCollection data)
         {
             int userId = int.Parse(User.FindFirst("userid").Value);
-            CustomerAccount ca = new CustomerAccount()
-            {
-                Comments = new List<CustomerAccountComment>(),
-            };
-
-            ca.AccountName = data["accountName"];
-            ca.IsVisible = Boolean.Parse(data["visibility"]);
-            ca.CreatedAt = _appDateTimeService.GetCurrentDateTime();
-            ca.CreatedById = userId;
-            ca.UpdatedAt = _appDateTimeService.GetCurrentDateTime();
-            ca.UpdatedById = userId;
-
-            if (!String.IsNullOrEmpty(data["accountComments"]))
-            {
-                CustomerAccountComment cac = new CustomerAccountComment();
-                cac.Comment = data["accountComments"].ToString().Trim();
-                cac.CustomerAccountId = ca.CustomerAccountId;
-                cac.CreatedAt = _appDateTimeService.GetCurrentDateTime();
-                cac.CreatedById = userId;
-                cac.ParentId = null;
-                cac.UpdatedAt = _appDateTimeService.GetCurrentDateTime();
-                ca.Comments.Add(cac);
-            }
-
-            AccountRate ar = new AccountRate();
-            ar.CustomerAccountId = ca.CustomerAccountId;
-            ar.RatePerHour = decimal.Parse(data["accountRate"]);
-            ar.EffectiveStartDate = DateTime.ParseExact(data["rateStartDate"], "d/M/yyyy", System.Globalization.CultureInfo.InvariantCulture);
-            ar.EffectiveEndDate = DateTime.ParseExact(data["rateEndDate"], "d/M/yyyy", System.Globalization.CultureInfo.InvariantCulture);
-            ar.CreatedAt = _appDateTimeService.GetCurrentDateTime();
-            ar.CreatedById = userId;
-            ar.UpdatedAt = _appDateTimeService.GetCurrentDateTime();
-            ar.UpdatedById = userId;
-            ca.AccountRates.Add(ar);
-
+            
+                CustomerAccount ca = new CustomerAccount()
+                {
+                    Comments = new List<CustomerAccountComment>(),
+                };
             try
             {
+                ca.AccountName = data["accountName"];
+                ca.IsVisible = Boolean.Parse(data["visibility"]);
+                ca.CreatedAt = _appDateTimeService.GetCurrentDateTime();
+                ca.CreatedById = userId;
+                ca.UpdatedAt = _appDateTimeService.GetCurrentDateTime();
+                ca.UpdatedById = userId;
+
+                if (!String.IsNullOrEmpty(data["accountComments"]))
+                {
+                    CustomerAccountComment cac = new CustomerAccountComment();
+                    cac.Comment = data["accountComments"].ToString().Trim();
+                    cac.CustomerAccountId = ca.CustomerAccountId;
+                    cac.CreatedAt = _appDateTimeService.GetCurrentDateTime();
+                    cac.CreatedById = userId;
+                    cac.ParentId = null;
+                    cac.UpdatedAt = _appDateTimeService.GetCurrentDateTime();
+                    ca.Comments.Add(cac);
+                }
+
+                AccountRate ar = new AccountRate();
+                ar.CustomerAccountId = ca.CustomerAccountId;
+                ar.RatePerHour = decimal.Parse(data["accountRate"]);
+                ar.EffectiveStartDate = DateTime.ParseExact(data["rateStartDate"], "d/M/yyyy", System.Globalization.CultureInfo.InvariantCulture);
+                ar.EffectiveEndDate = DateTime.ParseExact(data["rateEndDate"], "d/M/yyyy", System.Globalization.CultureInfo.InvariantCulture);
+                ar.CreatedAt = _appDateTimeService.GetCurrentDateTime();
+                ar.CreatedById = userId;
+                ar.UpdatedAt = _appDateTimeService.GetCurrentDateTime();
+                ar.UpdatedById = userId;
+                ca.AccountRates.Add(ar);
+
+
                 Database.CustomerAccounts.Add(ca);
                 Database.SaveChanges();
-                
-            }catch(Exception ex)
+
+            }
+            catch (SqlException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "Something wrong has occured. Please contact the administrators." });
+            }
+            catch (Exception ex)
             {
                 return BadRequest(new { message = ex.InnerException.Message });
             }
-            return Ok(new { message = "Successfully Registered "+ca.AccountName });
+            return Ok(new { message = "Successfully Registered " + ca.AccountName });
 
         }
 
