4127d764e45c193eb3ad6af44782e044fc0fbd88
Pencilate <kazukitsukino@gmail.com>
2019-12-15 16:44:57 +0800

    Implemented Account Rate Modal Backend functions

--- TMS/APIs/AccountRatesController.cs
+++ TMS/APIs/AccountRatesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
@@ -87,7 +88,7 @@ namespace TMS.APIs
 
                 totalPage = (int)Math.Ceiling((double)totalRecords / pageSize);
 
-                if(currentPage == totalPage)
+                if((currentPage == totalPage) && (totalRecords % pageSize != 0))
                 {
                     endRecord = startRecord + (totalRecords % pageSize) - 1;
                 }
@@ -141,65 +142,270 @@ namespace TMS.APIs
             }
         }
 
+        [Authorize("ADMIN")]
+        [HttpGet("{cusId}/{rateId}")]
+        public IActionResult GetRate(int cusId, int rateId)
+        {
+            try
+            {
+                AccountRate ar = Database.AccountRates.Where(c => c.CustomerAccountId == cusId).SingleOrDefault(r => r.AccountRateId == rateId);
+                if(ar == null)
+                {
+                    return NotFound(new { message = "Customer account rate could not be found" });
+
+                }
+                object arObject = new
+                {
+                    rate = ar.RatePerHour,
+                    startDate = ar.EffectiveStartDate,
+                    endDate = ar.EffectiveEndDate
+                };
+                return Ok(arObject);
+            }
+            catch (SqlException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "Something wrong has occured. Please contact the administrators." });
+            }
+        }
+
+        [Authorize("ADMIN")]
+        [HttpGet("RateSummary/{cusId}/{rateId}")]
+        public IActionResult GetAccountRateSummary(int cusId, int rateId)
+        {
+            try
+            {
+                AccountRate ar = Database.AccountRates.Where(c => c.CustomerAccountId == cusId).SingleOrDefault(r => r.AccountRateId == rateId);
+                if (ar == null)
+                {
+                    return NotFound(new { message = "Customer account rate could not be found" });
+                }
+                int ttCount = Database.AccountTimeTable.Where(t => t.AccountRateId == rateId).Count();
+
+                object arObject = new
+                {
+                    rate = ar.RatePerHour,
+                    startDate = ar.EffectiveStartDate,
+                    endDate = ar.EffectiveEndDate,
+                    ttCount = ttCount
+                };
+                return Ok(arObject);
+            }
+            catch (SqlException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "Something wrong has occured. Please contact the administrators." });
+            }
+        }
+
         // POST api/<controller>
+        [Authorize("ADMIN")]
         [HttpPost("Create/{id}")]
         public IActionResult Post(int id, [FromForm]IFormCollection data)
         {
-            int userId = int.Parse(User.FindFirst("userid").Value);
-
-            AccountRate newAR = new AccountRate();
-            newAR.CustomerAccountId = id;
-            newAR.EffectiveStartDate = DateTime.ParseExact(data["rateStartDate"], "d/M/yyyy", System.Globalization.CultureInfo.InvariantCulture);
-            newAR.EffectiveEndDate = DateTime.ParseExact(data["rateEndDate"], "d/M/yyyy", System.Globalization.CultureInfo.InvariantCulture);
-            newAR.RatePerHour = decimal.Parse(data["accountRate"]);
-            newAR.CreatedAt = _appDateTimeService.GetCurrentDateTime();
-            newAR.CreatedById = userId;
-            newAR.UpdatedAt = _appDateTimeService.GetCurrentDateTime();
-            newAR.UpdatedById = userId;
-
-            List<AccountRate> allAR = Database.AccountRates.Where(r => r.CustomerAccountId == id).ToList();
-            bool overlap = false;
-            foreach (AccountRate ar in allAR)
+            try
             {
-                bool overlapCurrentRecord = false;
-                if (newAR.EffectiveStartDate.CompareTo(ar.EffectiveStartDate) >= 0)//After
+                int userId = int.Parse(User.FindFirst("userid").Value);
+
+                AccountRate newAR = new AccountRate();
+                newAR.CustomerAccountId = id;
+                newAR.EffectiveStartDate = DateTime.ParseExact(data["rateStartDate"], "d/M/yyyy", System.Globalization.CultureInfo.InvariantCulture);
+                newAR.EffectiveEndDate = DateTime.ParseExact(data["rateEndDate"], "d/M/yyyy", System.Globalization.CultureInfo.InvariantCulture);
+                newAR.RatePerHour = decimal.Parse(data["accountRate"]);
+                newAR.CreatedAt = _appDateTimeService.GetCurrentDateTime();
+                newAR.CreatedById = userId;
+                newAR.UpdatedAt = _appDateTimeService.GetCurrentDateTime();
+                newAR.UpdatedById = userId;
+
+                List<AccountRate> allAR = Database.AccountRates.Where(r => r.CustomerAccountId == id).ToList();
+                bool overlap = false;
+                List<String> overlapMsg = new List<string>();
+                foreach (AccountRate ar in allAR)
                 {
-                    if(newAR.EffectiveEndDate.CompareTo(ar.EffectiveEndDate) <= 0)//Before
+                    bool overlapCurrentRecord = false;
+                    if (newAR.EffectiveStartDate.CompareTo(ar.EffectiveStartDate) >= 0)//After
                     {
-                        overlapCurrentRecord = true; //Record overlap the entire period
+                        if (newAR.EffectiveEndDate.CompareTo(ar.EffectiveEndDate) <= 0)//Before
+                        {
+                            overlapCurrentRecord = true; //Record overlap the entire period
+                        }
+                        if (newAR.EffectiveStartDate.CompareTo(ar.EffectiveEndDate) <= 0)//Before
+                        {
+                            overlapCurrentRecord = true; // Record overlap front of period
+                        }
+
                     }
-                    if (newAR.EffectiveStartDate.CompareTo(ar.EffectiveEndDate) <= 0)//Before
+                    else//Before
                     {
-                        overlapCurrentRecord = true; // Record overlap front of period
+                        if (newAR.EffectiveEndDate.CompareTo(ar.EffectiveEndDate) >= 0)//After
+                        {
+                            overlapCurrentRecord = true; //Record is encapulated by period
+                        }
+                        if (newAR.EffectiveEndDate.CompareTo(ar.EffectiveStartDate) >= 0)//After
+                        {
+                            overlapCurrentRecord = true; //Record over back of period
+                        }
+                    }
+                    if (overlapCurrentRecord)
+                    {
+                        overlap = true;
+                        overlapMsg.Add(ar.EffectiveStartDate.ToString("d") + " and " + ar.EffectiveEndDate.ToString("d"));
+                    }
+                }
+
+
+                if (overlap)
+                {
+                    String message = "Overlaps with existing rate dates! Dates cannot be between ";
+                    for (int i = 0; i < overlapMsg.Count; i++)
+                    {
+                        if (i > 0)
+                        {
+                            message += " or ";
+                        }
+                        message += overlapMsg[i] + " inclusive";
+                    }
+                    return BadRequest(new { message = message });
+                }
+                else
+                {
+                    foreach (AccountRate ar in allAR)
+                    {
+                        if ((newAR.RatePerHour == ar.RatePerHour) && ((newAR.EffectiveStartDate.AddDays(-1).Date.CompareTo(ar.EffectiveEndDate) == 0) || (newAR.EffectiveEndDate.AddDays(1).Date.CompareTo(ar.EffectiveStartDate) == 0)))
+                        {
+                            return BadRequest(new { message = "Extend your existing account rate! Existing account rate with the same rate/hour of $" + ar.RatePerHour + " is successive with your new account rate." });
+
+                        }
                     }
 
-                }
-                else//Before
-                {
-                    if (newAR.EffectiveEndDate.CompareTo(ar.EffectiveEndDate) >= 0)//After
-                    {
-                        overlapCurrentRecord = true; //Record is encapulated by period
-                    }
-                    if (newAR.EffectiveEndDate.CompareTo(ar.EffectiveStartDate) <= 0)//Before
-                    {
-                        overlapCurrentRecord = true; //Record over back of period
-                    }
+                    Database.AccountRates.Add(newAR);
+                    Database.SaveChanges();
+                    return Ok(new { message = "Successfully added record." });
+
                 }
             }
-
-            
+            catch (SqlException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "Something wrong has occured. Please contact the administrators." });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.InnerException.Message });
+            }
         }
 
         // PUT api/<controller>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        [Authorize("ADMIN")]
+        [HttpPut("{cusId}/{rateId}")]
+        public IActionResult UpdateCustomerAccountRate(int cusId, int rateId, [FromForm]IFormCollection data)
         {
+            try
+            {
+                int userId = int.Parse(User.FindFirst("userid").Value);
+
+                AccountRate arToUpdate = Database.AccountRates.Where(c => c.CustomerAccountId == cusId).SingleOrDefault(r => r.AccountRateId == rateId);
+                arToUpdate.EffectiveStartDate = DateTime.ParseExact(data["rateStartDate"], "d/M/yyyy", System.Globalization.CultureInfo.InvariantCulture);
+                arToUpdate.EffectiveEndDate = DateTime.ParseExact(data["rateEndDate"], "d/M/yyyy", System.Globalization.CultureInfo.InvariantCulture);
+                arToUpdate.RatePerHour = decimal.Parse(data["accountRate"]);
+                arToUpdate.UpdatedAt = _appDateTimeService.GetCurrentDateTime();
+                arToUpdate.UpdatedById = userId;
+
+                List<AccountRate> allAR = Database.AccountRates.Where(r => r.CustomerAccountId == cusId).Where(r => r.AccountRateId != rateId).ToList();
+                bool overlap = false;
+                List<String> overlapMsg = new List<string>();
+                foreach (AccountRate ar in allAR)
+                {
+                    bool overlapCurrentRecord = false;
+                    if (arToUpdate.EffectiveStartDate.CompareTo(ar.EffectiveStartDate) >= 0)//After
+                    {
+                        if (arToUpdate.EffectiveEndDate.CompareTo(ar.EffectiveEndDate) <= 0)//Before
+                        {
+                            overlapCurrentRecord = true; //Record overlap the entire period
+                        }
+                        if (arToUpdate.EffectiveStartDate.CompareTo(ar.EffectiveEndDate) <= 0)//Before
+                        {
+                            overlapCurrentRecord = true; // Record overlap front of period
+                        }
+
+                    }
+                    else//Before
+                    {
+                        if (arToUpdate.EffectiveEndDate.CompareTo(ar.EffectiveEndDate) >= 0)//After
+                        {
+                            overlapCurrentRecord = true; //Record is encapulated by period
+                        }
+                        if (arToUpdate.EffectiveEndDate.CompareTo(ar.EffectiveStartDate) >= 0)//After
+                        {
+                            overlapCurrentRecord = true; //Record over back of period
+                        }
+                    }
+                    if (overlapCurrentRecord)
+                    {
+                        overlap = true;
+                        overlapMsg.Add(ar.EffectiveStartDate.ToString("d") + " and " + ar.EffectiveEndDate.ToString("d"));
+                    }
+                }
+
+
+                if (overlap)
+                {
+                    String message = "Overlaps with other existing rate dates! Dates cannot be between ";
+                    for (int i = 0; i < overlapMsg.Count; i++)
+                    {
+                        if (i > 0)
+                        {
+                            message += " or ";
+                        }
+                        message += overlapMsg[i] + " inclusive";
+                    }
+                    return BadRequest(new { message = message });
+                }
+                else
+                {
+                    foreach (AccountRate ar in allAR)
+                    {
+                        if ((arToUpdate.RatePerHour == ar.RatePerHour) && ((arToUpdate.EffectiveStartDate.AddDays(-1).Date.CompareTo(ar.EffectiveEndDate) == 0) || (arToUpdate.EffectiveEndDate.AddDays(1).Date.CompareTo(ar.EffectiveStartDate) == 0)))
+                        {
+                            return BadRequest(new { message = "Extend your other existing account rate! Other existing account rate with the same rate/hour of $" + ar.RatePerHour + " is successive with your new account rate." });
+
+                        }
+                    }
+                    Database.AccountRates.Update(arToUpdate);
+                    Database.SaveChanges();
+                    return Ok(new { message = "Successfully updated record." });
+
+                }
+            }
+            catch (SqlException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "Something wrong has occured. Please contact the administrators." });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.InnerException.Message });
+            }
         }
 
         // DELETE api/<controller>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [Authorize("ADMIN")]
+        [HttpDelete("{cusId}/{rateId}")]
+        public IActionResult Delete(int cusId, int rateId)
         {
+            try
+            {
+                AccountRate ar = Database.AccountRates.Where(r => r.CustomerAccountId == cusId).SingleOrDefault(r => r.AccountRateId == rateId);
+                if(ar == null)
+                {
+                    return NotFound(new { message = "Customer account rate could not be found" });
+                }
+                List<AccountTimeTable> tt = Database.AccountTimeTable.Where(t => t.AccountRateId == rateId).ToList();
+                Database.AccountTimeTable.RemoveRange(tt);
+                Database.AccountRates.Remove(ar);
+                Database.SaveChanges();
+                return Ok(new { message = "Successfully Deleted Account Rate record with acommpanying TimeTable records" });
+            }
+            catch (SqlException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "Something wrong has occured. Please contact the administrators." });
+            }
         }
     }
 
