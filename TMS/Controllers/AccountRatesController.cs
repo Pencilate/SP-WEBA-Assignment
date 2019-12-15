4f31f7ab2f29d0f699bbf323c03155b807b8b15c
Pencilate <kazukitsukino@gmail.com>
2019-12-15 22:22:40 +0800

    Added Security Role verification for the backend. Configured the  datepicker library to restrict the end date selection range

--- TMS/Controllers/AccountRatesController.cs
+++ TMS/Controllers/AccountRatesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,10 +12,13 @@ namespace TMS.Controllers
     public class AccountRatesController : Controller
     {
         // GET: /<controller>/
+        [Authorize("ADMIN")]
         public IActionResult Index()
         {
             return View();
         }
+
+        [Authorize("ADMIN")]
         public IActionResult Create()
         {
             return View();
--- TMS/Controllers/CustomerAccountsController.cs
+++ TMS/Controllers/CustomerAccountsController.cs
@@ -2,18 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TMS.Controllers
 {
 	public class CustomerAccountsController : Controller
 	{
-		// GET HTTP request, <base address>/CustomerAccounts/Index
-		public IActionResult Index()
+        // GET HTTP request, <base address>/CustomerAccounts/Index
+        [Authorize("ADMIN")]
+        public IActionResult Index()
 		{
 			return View();
 		}
 
+        [Authorize("ADMIN")]
         public IActionResult Create()
         {
             return View();
